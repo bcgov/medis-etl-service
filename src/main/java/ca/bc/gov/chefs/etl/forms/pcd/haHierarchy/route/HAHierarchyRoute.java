5daaa8e6a72353a5fa86f76124f7a22bce7c6490
SimoChami <simo.chami@cgi.com>
2023-12-22 15:32:20 -0500

    network endpoint setup for ha hierarchy

---
+++ src/main/java/ca/bc/gov/chefs/etl/forms/pcd/haHierarchy/route/HAHierarchyRoute.java
@@ -0,0 +1,35 @@
+package ca.bc.gov.chefs.etl.forms.pcd.haHierarchy.route;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ca.bc.gov.chefs.etl.core.routes.BaseRoute;
+import ca.bc.gov.chefs.etl.forms.pcd.haHierarchy.processor.PcdHAHierarchyApiProcessor;
+import ca.bc.gov.chefs.etl.forms.pcd.haHierarchy.processor.PcdHAHierarchyApiResponseProcessor;
+
+public class HAHierarchyRoute extends BaseRoute {
+
+	private static final Logger logger = LoggerFactory.getLogger(HAHierarchyRoute.class);
+	@Override
+	public void configure() throws Exception {
+		super.configure();
+		logger.info("Loaded HA Hierarchy Route");
+
+		/**
+		 * receive JSON payload, parse and set to make an API call
+		 */
+		// trigger
+		from("jetty:http://{{hostname}}:{{port}}/pcdbi/ha-hierarchy").routeId("pcdbi-ha-hierarchy-form")
+				.log("CHEFS-ETL received a request for HA Hierarchy Form extraction")
+				.to("direct:pcdbi-ha-hierarchy").end();
+
+		from("direct:pcdbi-ha-hierarchy")
+				// to the http uri
+				.process(new PcdHAHierarchyApiProcessor())
+				.toD("${header.RequestUri}")
+				.log("This is the status code from the response: ${header.CamelHttpResponseCode}")
+				.log("Trying to convert the received body OK").convertBodyTo(String.class)
+				.process(new PcdHAHierarchyApiResponseProcessor()).end();
+	}
+
+}
