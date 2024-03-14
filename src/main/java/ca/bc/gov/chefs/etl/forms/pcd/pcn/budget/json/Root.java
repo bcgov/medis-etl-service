bfbfe6884a638384e91b696025eb3acca0af383a
SimoChami <simo.chami@cgi.com>
2024-03-14 15:51:11 -0400

    all payload aprsing done

--- src/main/java/ca/bc/gov/chefs/etl/forms/pcd/pcn/budget/json/Root.java
+++ src/main/java/ca/bc/gov/chefs/etl/forms/pcd/pcn/budget/json/Root.java
@@ -1,5 +1,7 @@
 package ca.bc.gov.chefs.etl.forms.pcd.pcn.budget.json;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import ca.bc.gov.chefs.etl.core.json.Form;
@@ -7,4 +9,53 @@ import ca.bc.gov.chefs.etl.core.json.Form;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Root {
     public Form form;
+    public RootTotals totals;
+    public String lateEntry;
+    public String fiscalYear;
+    public String communityName;
+    public String healthAuthority;
+    public List<RootPcnBudgetItem> pcnBudget;
+    
+    public Form getForm() {
+        return form;
+    }
+    public void setForm(Form form) {
+        this.form = form;
+    }
+    public RootTotals getTotals() {
+        return totals;
+    }
+    public void setTotals(RootTotals totals) {
+        this.totals = totals;
+    }
+    public String getLateEntry() {
+        return lateEntry;
+    }
+    public void setLateEntry(String lateEntry) {
+        this.lateEntry = lateEntry;
+    }
+    public String getFiscalYear() {
+        return fiscalYear;
+    }
+    public void setFiscalYear(String fiscalYear) {
+        this.fiscalYear = fiscalYear;
+    }
+    public String getCommunityName() {
+        return communityName;
+    }
+    public void setCommunityName(String communityName) {
+        this.communityName = communityName;
+    }
+    public String getHealthAuthority() {
+        return healthAuthority;
+    }
+    public void setHealthAuthority(String healthAuthority) {
+        this.healthAuthority = healthAuthority;
+    }
+    public List<RootPcnBudgetItem> getPcnBudget() {
+        return pcnBudget;
+    }
+    public void setPcnBudget(List<RootPcnBudgetItem> pcnBudget) {
+        this.pcnBudget = pcnBudget;
+    }
 }
---
+++ src/main/java/ca/bc/gov/chefs/etl/forms/pcd/pcn/budget/json/RootTotals.java
@@ -0,0 +1,218 @@
+package ca.bc.gov.chefs.etl.forms.pcd.pcn.budget.json;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class RootTotals {
+    public String dofpApprovedFtes;
+    public String totalApprovedFtes;
+    public String dofpApprovedBudget; 
+    public String dofpFiscalYearFtes;
+    public String totalApprovedFtes1;
+    public String totalApprovedBudget;
+    public String clinicalApprovedFtes;
+    public String dofpBudgetAllocation;
+    public String totalApprovedBudget1;
+    public String physicianApprovedFtes;
+    public String clinicalApprovedBudget;
+    public String clinicalFiscalYearFtes;
+    public String overheadApprovedBudget;
+    public String overheadDofpAllocation;
+    public String dofpResourcesAllocation;
+    public String physicianApprovedBudget;
+    public String physicianFiscalYearFtes;
+    public String clinicalBudgetAllocation;
+    public String overheadBudgetAllocation;
+    public String dofpResourcesApprovedFtes;
+    public String physicianBudgetAllocation;
+    public String overheadDofpApprovedBudget;
+    public String dofpResourcesApprovedBudget; 
+    public String dofpResourcesFiscalYearFtes;
+    public String healthAuthorityApprovedFtes; 
+    public String oneTimeFundingDofpAllocation;
+    public String healthAuthorityApprovedBudget;
+    public String healthAuthorityFiscalYearFtes;
+    public String oneTimeFundingBudgetAllocation;
+    public String healthAuthorityBudgetAllocation;
+
+    public String getDofpApprovedFtes() {
+        return dofpApprovedFtes;
+    }
+    public void setDofpApprovedFtes(String dofpApprovedFtes) {
+        this.dofpApprovedFtes = dofpApprovedFtes;
+    }
+    public String getTotalApprovedFtes() {
+        return totalApprovedFtes;
+    }
+    public void setTotalApprovedFtes(String totalApprovedFtes) {
+        this.totalApprovedFtes = totalApprovedFtes;
+    }
+    public String getDofpApprovedBudget() {
+        return dofpApprovedBudget;
+    }
+    public void setDofpApprovedBudget(String dofpApprovedBudget) {
+        this.dofpApprovedBudget = dofpApprovedBudget;
+    }
+    public String getDofpFiscalYearFtes() {
+        return dofpFiscalYearFtes;
+    }
+    public void setDofpFiscalYearFtes(String dofpFiscalYearFtes) {
+        this.dofpFiscalYearFtes = dofpFiscalYearFtes;
+    }
+    public String getTotalApprovedFtes1() {
+        return totalApprovedFtes1;
+    }
+    public void setTotalApprovedFtes1(String totalApprovedFtes1) {
+        this.totalApprovedFtes1 = totalApprovedFtes1;
+    }
+    public String getTotalApprovedBudget() {
+        return totalApprovedBudget;
+    }
+    public void setTotalApprovedBudget(String totalApprovedBudget) {
+        this.totalApprovedBudget = totalApprovedBudget;
+    }
+    public String getClinicalApprovedFtes() {
+        return clinicalApprovedFtes;
+    }
+    public void setClinicalApprovedFtes(String clinicalApprovedFtes) {
+        this.clinicalApprovedFtes = clinicalApprovedFtes;
+    }
+    public String getDofpBudgetAllocation() {
+        return dofpBudgetAllocation;
+    }
+    public void setDofpBudgetAllocation(String dofpBudgetAllocation) {
+        this.dofpBudgetAllocation = dofpBudgetAllocation;
+    }
+    public String getTotalApprovedBudget1() {
+        return totalApprovedBudget1;
+    }
+    public void setTotalApprovedBudget1(String totalApprovedBudget1) {
+        this.totalApprovedBudget1 = totalApprovedBudget1;
+    }
+    public String getPhysicianApprovedFtes() {
+        return physicianApprovedFtes;
+    }
+    public void setPhysicianApprovedFtes(String physicianApprovedFtes) {
+        this.physicianApprovedFtes = physicianApprovedFtes;
+    }
+    public String getClinicalApprovedBudget() {
+        return clinicalApprovedBudget;
+    }
+    public void setClinicalApprovedBudget(String clinicalApprovedBudget) {
+        this.clinicalApprovedBudget = clinicalApprovedBudget;
+    }
+    public String getClinicalFiscalYearFtes() {
+        return clinicalFiscalYearFtes;
+    }
+    public void setClinicalFiscalYearFtes(String clinicalFiscalYearFtes) {
+        this.clinicalFiscalYearFtes = clinicalFiscalYearFtes;
+    }
+    public String getOverheadApprovedBudget() {
+        return overheadApprovedBudget;
+    }
+    public void setOverheadApprovedBudget(String overheadApprovedBudget) {
+        this.overheadApprovedBudget = overheadApprovedBudget;
+    }
+    public String getOverheadDofpAllocation() {
+        return overheadDofpAllocation;
+    }
+    public void setOverheadDofpAllocation(String overheadDofpAllocation) {
+        this.overheadDofpAllocation = overheadDofpAllocation;
+    }
+    public String getDofpResourcesAllocation() {
+        return dofpResourcesAllocation;
+    }
+    public void setDofpResourcesAllocation(String dofpResourcesAllocation) {
+        this.dofpResourcesAllocation = dofpResourcesAllocation;
+    }
+    public String getPhysicianApprovedBudget() {
+        return physicianApprovedBudget;
+    }
+    public void setPhysicianApprovedBudget(String physicianApprovedBudget) {
+        this.physicianApprovedBudget = physicianApprovedBudget;
+    }
+    public String getPhysicianFiscalYearFtes() {
+        return physicianFiscalYearFtes;
+    }
+    public void setPhysicianFiscalYearFtes(String physicianFiscalYearFtes) {
+        this.physicianFiscalYearFtes = physicianFiscalYearFtes;
+    }
+    public String getClinicalBudgetAllocation() {
+        return clinicalBudgetAllocation;
+    }
+    public void setClinicalBudgetAllocation(String clinicalBudgetAllocation) {
+        this.clinicalBudgetAllocation = clinicalBudgetAllocation;
+    }
+    public String getOverheadBudgetAllocation() {
+        return overheadBudgetAllocation;
+    }
+    public void setOverheadBudgetAllocation(String overheadBudgetAllocation) {
+        this.overheadBudgetAllocation = overheadBudgetAllocation;
+    }
+    public String getDofpResourcesApprovedFtes() {
+        return dofpResourcesApprovedFtes;
+    }
+    public void setDofpResourcesApprovedFtes(String dofpResourcesApprovedFtes) {
+        this.dofpResourcesApprovedFtes = dofpResourcesApprovedFtes;
+    }
+    public String getPhysicianBudgetAllocation() {
+        return physicianBudgetAllocation;
+    }
+    public void setPhysicianBudgetAllocation(String physicianBudgetAllocation) {
+        this.physicianBudgetAllocation = physicianBudgetAllocation;
+    }
+    public String getOverheadDofpApprovedBudget() {
+        return overheadDofpApprovedBudget;
+    }
+    public void setOverheadDofpApprovedBudget(String overheadDofpApprovedBudget) {
+        this.overheadDofpApprovedBudget = overheadDofpApprovedBudget;
+    }
+    public String getDofpResourcesApprovedBudget() {
+        return dofpResourcesApprovedBudget;
+    }
+    public void setDofpResourcesApprovedBudget(String dofpResourcesApprovedBudget) {
+        this.dofpResourcesApprovedBudget = dofpResourcesApprovedBudget;
+    }
+    public String getDofpResourcesFiscalYearFtes() {
+        return dofpResourcesFiscalYearFtes;
+    }
+    public void setDofpResourcesFiscalYearFtes(String dofpResourcesFiscalYearFtes) {
+        this.dofpResourcesFiscalYearFtes = dofpResourcesFiscalYearFtes;
+    }
+    public String getHealthAuthorityApprovedFtes() {
+        return healthAuthorityApprovedFtes;
+    }
+    public void setHealthAuthorityApprovedFtes(String healthAuthorityApprovedFtes) {
+        this.healthAuthorityApprovedFtes = healthAuthorityApprovedFtes;
+    }
+    public String getOneTimeFundingDofpAllocation() {
+        return oneTimeFundingDofpAllocation;
+    }
+    public void setOneTimeFundingDofpAllocation(String oneTimeFundingDofpAllocation) {
+        this.oneTimeFundingDofpAllocation = oneTimeFundingDofpAllocation;
+    }
+    public String getHealthAuthorityApprovedBudget() {
+        return healthAuthorityApprovedBudget;
+    }
+    public void setHealthAuthorityApprovedBudget(String healthAuthorityApprovedBudget) {
+        this.healthAuthorityApprovedBudget = healthAuthorityApprovedBudget;
+    }
+    public String getHealthAuthorityFiscalYearFtes() {
+        return healthAuthorityFiscalYearFtes;
+    }
+    public void setHealthAuthorityFiscalYearFtes(String healthAuthorityFiscalYearFtes) {
+        this.healthAuthorityFiscalYearFtes = healthAuthorityFiscalYearFtes;
+    }
+    public String getOneTimeFundingBudgetAllocation() {
+        return oneTimeFundingBudgetAllocation;
+    }
+    public void setOneTimeFundingBudgetAllocation(String oneTimeFundingBudgetAllocation) {
+        this.oneTimeFundingBudgetAllocation = oneTimeFundingBudgetAllocation;
+    }
+    public String getHealthAuthorityBudgetAllocation() {
+        return healthAuthorityBudgetAllocation;
+    }
+    public void setHealthAuthorityBudgetAllocation(String healthAuthorityBudgetAllocation) {
+        this.healthAuthorityBudgetAllocation = healthAuthorityBudgetAllocation;
+    }
+}
