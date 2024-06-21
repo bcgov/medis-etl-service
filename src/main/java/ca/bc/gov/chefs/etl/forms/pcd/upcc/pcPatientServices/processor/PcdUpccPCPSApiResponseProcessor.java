6c7d72ca6dea2d5a8c72b120d5e28dd10b1f6a97
Dinh Nguyen Pham <nguyenphamswork@gmail.com>
2024-06-21 15:58:09 -0700

    feat: refactoring getPeriodicField to JsonUtil and removing redundant toString() calls

--- src/main/java/ca/bc/gov/chefs/etl/forms/pcd/upcc/pcPatientServices/processor/PcdUpccPCPSApiResponseProcessor.java
+++ src/main/java/ca/bc/gov/chefs/etl/forms/pcd/upcc/pcPatientServices/processor/PcdUpccPCPSApiResponseProcessor.java
@@ -19,6 +19,7 @@ import ca.bc.gov.chefs.etl.forms.pcd.upcc.pcPatientServices.model.PCPSUpccSubmis
 import ca.bc.gov.chefs.etl.forms.pcd.upcc.pcPatientServices.model.PCPSUpccSubmissionData;
 import ca.bc.gov.chefs.etl.util.CSVUtil;
 import ca.bc.gov.chefs.etl.util.FileUtil;
+import ca.bc.gov.chefs.etl.util.JsonUtil;
 
 public class PcdUpccPCPSApiResponseProcessor extends BaseApiResponseProcessor {
 
@@ -37,35 +38,33 @@ public class PcdUpccPCPSApiResponseProcessor extends BaseApiResponseProcessor {
         Map<String, List<List<String>>> map = CSVUtil.provider(iModels);
 
         boolean isHeaderAdded = (boolean) exchange.getProperties().get(Constants.IS_HEADER_ADDED);
-        List<String> filesGenerated = FileUtil.writeToCSVFile(map, PCDConstants.PCD_UPCC_PCPS_DIR, isHeaderAdded);
+        List<String> filesGenerated = FileUtil.writeToCSVFile(map, PCDConstants.PCD_UPCC_PCPS_DIR,
+                isHeaderAdded);
 
         SuccessResponse successResponse = new SuccessResponse();
         successResponse.setFiles(filesGenerated);
         exchange.getIn().setBody(mapper.writeValueAsString(successResponse));
     }
 
-    private Object getPeriodicField(Object o, String name, String index)
-            throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException {
-        return o.getClass().getDeclaredField(name + "P" + index).get(o);
-    }
-
-    private Boolean isPeriodEmpty(Object o, String index)
-            throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException {
-        return getPeriodicField(o, "uniquePatients", index.toString()) == null
-                && getPeriodicField(o, "uniquePatientsSinceOpening", index.toString()) == null
-                && getPeriodicField(o, "attachedToTheClinic", index.toString()) == null
-                && getPeriodicField(o, "attachedNotToTheClinic", index.toString()) == null
-                && getPeriodicField(o, "unattached", index.toString()) == null
-                && getPeriodicField(o, "deliveredVirtuallyPrac", index.toString()) == null
-                && getPeriodicField(o, "duringBusinessHoursPrac", index.toString()) == null
-                && getPeriodicField(o, "outsideBusinessHoursPrac", index.toString()) == null
-                && getPeriodicField(o, "deliveredVirtuallyNonPrac", index.toString()) == null
-                && getPeriodicField(o, "duringBusinessHoursNonPrac", index.toString()) == null
-                && getPeriodicField(o, "outsideBusinessHoursNonPrac", index.toString()) == null;
+    private Boolean isPeriodEmpty(Object o, Integer index)
+            throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException,
+            SecurityException {
+        return JsonUtil.getPeriodicField(o, "uniquePatients", index) == null
+                && JsonUtil.getPeriodicField(o, "uniquePatientsSinceOpening", index) == null
+                && JsonUtil.getPeriodicField(o, "attachedToTheClinic", index) == null
+                && JsonUtil.getPeriodicField(o, "attachedNotToTheClinic", index) == null
+                && JsonUtil.getPeriodicField(o, "unattached", index) == null
+                && JsonUtil.getPeriodicField(o, "deliveredVirtuallyPrac", index) == null
+                && JsonUtil.getPeriodicField(o, "duringBusinessHoursPrac", index) == null
+                && JsonUtil.getPeriodicField(o, "outsideBusinessHoursPrac", index) == null
+                && JsonUtil.getPeriodicField(o, "deliveredVirtuallyNonPrac", index) == null
+                && JsonUtil.getPeriodicField(o, "duringBusinessHoursNonPrac", index) == null
+                && JsonUtil.getPeriodicField(o, "outsideBusinessHoursNonPrac", index) == null;
     }
 
     private List<PCPSUpccSubmission> parsePCPSRequest(List<Root> pcpsModels)
-            throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException {
+            throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException,
+            SecurityException {
         List<PCPSUpccSubmission> parsedUpccPCPS = new ArrayList<>();
         for (Root root : pcpsModels) {
             PCPSUpccSubmission pcpsSubmission = new PCPSUpccSubmission();
@@ -96,7 +95,7 @@ public class PcdUpccPCPSApiResponseProcessor extends BaseApiResponseProcessor {
                 Object dataSubmissionObject = root.getDataSubmission();
 
                 // Check if all fields are null then skip the record
-                if (isPeriodEmpty(dataSubmissionObject, i.toString())) {
+                if (isPeriodEmpty(dataSubmissionObject, i)) {
                     continue;
                 }
 
@@ -104,68 +103,49 @@ public class PcdUpccPCPSApiResponseProcessor extends BaseApiResponseProcessor {
                 pcpsSubmissionData.setPcPatientServicesRecordId(java.util.UUID.randomUUID().toString());
                 pcpsSubmissionData.setPeriodForDataEntry(i.toString());
 
-                if (getPeriodicField(dataSubmissionObject, "uniquePatients", i.toString()) != null) {
+                if (JsonUtil.getPeriodicField(dataSubmissionObject, "uniquePatients", i) != null) {
                     pcpsSubmissionData
-                            .setUniquePatients(
-                                    getPeriodicField(dataSubmissionObject, "uniquePatients", i.toString()).toString());
+                            .setUniquePatients(JsonUtil.getPeriodicField(dataSubmissionObject, "uniquePatients", i));
                 }
-                if (getPeriodicField(dataSubmissionObject, "uniquePatientsSinceOpening", i.toString()) != null) {
-                    pcpsSubmissionData
-                            .setUniquePatientsSinceOpen(
-                                    getPeriodicField(dataSubmissionObject, "uniquePatientsSinceOpening", i.toString())
-                                            .toString());
+                if (JsonUtil.getPeriodicField(dataSubmissionObject, "uniquePatientsSinceOpening", i) != null) {
+                    pcpsSubmissionData.setUniquePatientsSinceOpen(
+                            JsonUtil.getPeriodicField(dataSubmissionObject, "uniquePatientsSinceOpening", i));
                 }
-                if (getPeriodicField(dataSubmissionObject, "attachedToTheClinic", i.toString()) != null) {
-                    pcpsSubmissionData
-                            .setPvAttachedToClinic(
-                                    getPeriodicField(dataSubmissionObject, "attachedToTheClinic", i.toString())
-                                            .toString());
+                if (JsonUtil.getPeriodicField(dataSubmissionObject, "attachedToTheClinic", i) != null) {
+                    pcpsSubmissionData.setPvAttachedToClinic(
+                            JsonUtil.getPeriodicField(dataSubmissionObject, "attachedToTheClinic", i));
                 }
-                if (getPeriodicField(dataSubmissionObject, "attachedNotToTheClinic", i.toString()) != null) {
-                    pcpsSubmissionData
-                            .setPvAttachedNotToClinic(
-                                    getPeriodicField(dataSubmissionObject, "attachedNotToTheClinic", i.toString())
-                                            .toString());
+                if (JsonUtil.getPeriodicField(dataSubmissionObject, "attachedNotToTheClinic", i) != null) {
+                    pcpsSubmissionData.setPvAttachedNotToClinic(
+                            JsonUtil.getPeriodicField(dataSubmissionObject, "attachedNotToTheClinic", i));
                 }
-                if (getPeriodicField(dataSubmissionObject, "unattached", i.toString()) != null) {
+                if (JsonUtil.getPeriodicField(dataSubmissionObject, "unattached", i) != null) {
                     pcpsSubmissionData.setPvUnattached(
-                            getPeriodicField(dataSubmissionObject, "unattached", i.toString()).toString());
+                            JsonUtil.getPeriodicField(dataSubmissionObject, "unattached", i));
                 }
-                if (getPeriodicField(dataSubmissionObject, "deliveredVirtuallyPrac", i.toString()) != null) {
-                    pcpsSubmissionData
-                            .setPeVirtuallyPrac(
-                                    getPeriodicField(dataSubmissionObject, "deliveredVirtuallyPrac", i.toString())
-                                            .toString());
+                if (JsonUtil.getPeriodicField(dataSubmissionObject, "deliveredVirtuallyPrac", i) != null) {
+                    pcpsSubmissionData.setPeVirtuallyPrac(
+                            JsonUtil.getPeriodicField(dataSubmissionObject, "deliveredVirtuallyPrac", i));
                 }
-                if (getPeriodicField(dataSubmissionObject, "duringBusinessHoursPrac", i.toString()) != null) {
-                    pcpsSubmissionData
-                            .setPeDuringBusHrsPrac(
-                                    getPeriodicField(dataSubmissionObject, "duringBusinessHoursPrac", i.toString())
-                                            .toString());
+                if (JsonUtil.getPeriodicField(dataSubmissionObject, "duringBusinessHoursPrac", i) != null) {
+                    pcpsSubmissionData.setPeDuringBusHrsPrac(
+                            JsonUtil.getPeriodicField(dataSubmissionObject, "duringBusinessHoursPrac", i));
                 }
-                if (getPeriodicField(dataSubmissionObject, "outsideBusinessHoursPrac", i.toString()) != null) {
-                    pcpsSubmissionData
-                            .setPeOutsideBusHrsPrac(
-                                    getPeriodicField(dataSubmissionObject, "outsideBusinessHoursPrac", i.toString())
-                                            .toString());
+                if (JsonUtil.getPeriodicField(dataSubmissionObject, "outsideBusinessHoursPrac", i) != null) {
+                    pcpsSubmissionData.setPeOutsideBusHrsPrac(
+                            JsonUtil.getPeriodicField(dataSubmissionObject, "outsideBusinessHoursPrac", i));
                 }
-                if (getPeriodicField(dataSubmissionObject, "deliveredVirtuallyNonPrac", i.toString()) != null) {
-                    pcpsSubmissionData
-                            .setPeVirtuallyNonPrac(
-                                    getPeriodicField(dataSubmissionObject, "deliveredVirtuallyNonPrac", i.toString())
-                                            .toString());
+                if (JsonUtil.getPeriodicField(dataSubmissionObject, "deliveredVirtuallyNonPrac", i) != null) {
+                    pcpsSubmissionData.setPeVirtuallyNonPrac(
+                            JsonUtil.getPeriodicField(dataSubmissionObject, "deliveredVirtuallyNonPrac", i));
                 }
-                if (getPeriodicField(dataSubmissionObject, "duringBusinessHoursNonPrac", i.toString()) != null) {
-                    pcpsSubmissionData
-                            .setPeDuringBusHrsNonPrac(
-                                    getPeriodicField(dataSubmissionObject, "duringBusinessHoursNonPrac", i.toString())
-                                            .toString());
+                if (JsonUtil.getPeriodicField(dataSubmissionObject, "duringBusinessHoursNonPrac", i) != null) {
+                    pcpsSubmissionData.setPeDuringBusHrsNonPrac(
+                            JsonUtil.getPeriodicField(dataSubmissionObject, "duringBusinessHoursNonPrac", i));
                 }
-                if (getPeriodicField(dataSubmissionObject, "outsideBusinessHoursNonPrac", i.toString()) != null) {
-                    pcpsSubmissionData
-                            .setPeOutsideBusHrsNonPrac(
-                                    getPeriodicField(dataSubmissionObject, "outsideBusinessHoursNonPrac", i.toString())
-                                            .toString());
+                if (JsonUtil.getPeriodicField(dataSubmissionObject, "outsideBusinessHoursNonPrac", i) != null) {
+                    pcpsSubmissionData.setPeOutsideBusHrsNonPrac(
+                            JsonUtil.getPeriodicField(dataSubmissionObject, "outsideBusinessHoursNonPrac", i));
                 }
                 pcpsSubmissionDataList.add(pcpsSubmissionData);
             }
--- src/main/java/ca/bc/gov/chefs/etl/util/JsonUtil.java
+++ src/main/java/ca/bc/gov/chefs/etl/util/JsonUtil.java
@@ -11,45 +11,58 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import ca.bc.gov.chefs.etl.constant.Constants;
 
 public class JsonUtil {
-	private static final ObjectMapper mapper = new ObjectMapper();
+    private static final ObjectMapper mapper = new ObjectMapper();
 
-	public static <T> T parseJsonString(String json, Class<T> clazz) throws Exception {
+    public static <T> T parseJsonString(String json, Class<T> clazz) throws Exception {
         return mapper.readValue(json, clazz);
     }
-	
-	public static String preProcess(String payload){
-		// The following code aims to replace occurences of "subTypeX":"" with "subTypeX":{}, as "subTypeX" is expected to be
-		// an object (can be empty) and not a String. 
-		return payload.replaceAll("\"(subType\\d*)\":\"\"", "\"$1\":{}");
-	}
 
-	public static String normalizeEmptyStringArrays(String payload){
-		// The following code aims to replace occurences of "nppcc":"[{}]" with "nppcc":[], as "nppcc" is expected to be
-		// a String array and not an object. 
+    public static String preProcess(String payload) {
+        // The following code aims to replace occurences of "subTypeX":"" with
+        // "subTypeX":{}, as "subTypeX" is expected to be
+        // an object (can be empty) and not a String.
+        return payload.replaceAll("\"(subType\\d*)\":\"\"", "\"$1\":{}");
+    }
+
+    public static String getPeriodicField(Object o, String name, Integer index)
+            throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException {
+        //  The following code aims to make it possible to dynamically access a field with
+        //  a Period suffix. For example, if the name is "uniquePatients" and the period is "P1",
+        //  the code will access the field "uniquePatientsP1" of the object.
+        if (o.getClass().getDeclaredField(name + "P" + index).get(o) == null) {
+            return null;
+        }
+        return o.getClass().getDeclaredField(name + "P" + index).get(o).toString();
+    }
+
+    public static String normalizeEmptyStringArrays(String payload) {
+        // The following code aims to replace occurences of "nppcc":"[{}]" with
+        // "nppcc":[], as "nppcc" is expected to be
+        // a String array and not an object.
         String NppccPattern = "\"nppccName\":\\[\\{\\}\\]";
         String NppccReplacement = "\"nppccName\": []";
 
-        String pcnPattern =  "\"pcnNames\":\\[\\{\\}\\]";
+        String pcnPattern = "\"pcnNames\":\\[\\{\\}\\]";
         String pcnReplacement = "\"pcnNames\": []";
 
-        String upccPattern =  "\"upccName\":\\[\\{\\}\\]";
+        String upccPattern = "\"upccName\":\\[\\{\\}\\]";
         String upccReplacement = "\"upccName\": []";
 
-        String chcPattern =  "\"chcName\":\\[\\{\\}\\]";
+        String chcPattern = "\"chcName\":\\[\\{\\}\\]";
         String chcReplacement = "\"chcName\": []";
 
-        String fnpccPattern =  "\"fnpccName\":\\[\\{\\}\\]";
+        String fnpccPattern = "\"fnpccName\":\\[\\{\\}\\]";
         String fnpccReplacement = "\"fnpccName\": []";
 
-		String result = payload.replaceAll(NppccPattern, NppccReplacement);
+        String result = payload.replaceAll(NppccPattern, NppccReplacement);
         result = result.replaceAll(pcnPattern, pcnReplacement);
         result = result.replaceAll(upccPattern, upccReplacement);
         result = result.replaceAll(chcPattern, chcReplacement);
         result = result.replaceAll(fnpccPattern, fnpccReplacement);
-		return result;
-	}
+        return result;
+    }
 
-    public static String ltcYTDBackwardCompatibility(String payload){
+    public static String ltcYTDBackwardCompatibility(String payload) {
         for (Map.Entry<String, String> entry : Constants.LTC_YTD_OLD_KEYS_COMPATIBILITY.entrySet()) {
             String key = entry.getKey();
             String value = entry.getValue();
@@ -58,18 +71,19 @@ public class JsonUtil {
         return payload;
     }
 
-	public static String roundDigitsNumber(String payload){
+    public static String roundDigitsNumber(String payload) {
 
-        // Regular expression pattern to match numbers with at least one digit before the decimal point and at least three digits after the decimal point.
+        // Regular expression pattern to match numbers with at least one digit before
+        // the decimal point and at least three digits after the decimal point.
         String pattern = "\\b\\d+\\.\\d{3,}\\b";
-        
+
         Pattern regex = Pattern.compile(pattern);
         Matcher matcher = regex.matcher(payload);
 
         // Iterate over matches and replace with numbers with 2 decimal places
         StringBuffer output = new StringBuffer();
         while (matcher.find()) {
-            //String matchedNumber = matcher.group();
+            // String matchedNumber = matcher.group();
             BigDecimal matchedNumber = new BigDecimal(matcher.group());
             BigDecimal roundedNumber = matchedNumber.setScale(2, RoundingMode.HALF_UP);
             String roundedString = roundedNumber.toString();
@@ -77,29 +91,33 @@ public class JsonUtil {
         }
         matcher.appendTail(output);
 
-		return output.toString();
-	}
-
-    public static String fixExpenseItemAndSubType(String payload){
-        // The following code aims to replace occurrences of  "expenseItem": {} with  "expenseItem": "", as "expenseItem"
-        // is expected to be a String and not an object. 
-        String result = payload.replaceAll("(\"expenseItem\":\\s*)\\{\\}", "$1\"\"");
-        // The following code aims to replace occurrences of  "expenseItemSubType": {} with  "expenseItemSubType": "", as "expenseItemSubType"
-        // is expected to be a String and not an object. 		
-		result = result.replaceAll("(\"expenseItemSubType\":\\s*)\\{\\}", "$1\"\""); 
-		return result;
-	}
-    
-    public static String ltcFacilityBackwardCompatibility(String payload){
-		String regexOwnerAddress = "\"ownerAddress\":\\s*\"([^\"]*)\"";
-        String replacementOwnerAddress = "\"ownerAddress\": {\"geometry\": {\"coordinates\": [0, 0]}, \"properties\": {\"fullAddress\": \"$1\"}}";
-		String regexOperatorAddress = "\"operatorAddress\":\\s*\"([^\"]*)\"";
-        String replacementOperatotAddress = "\"operatorAddress\": {\"geometry\": {\"coordinates\": [0, 0]}, \"properties\": {\"fullAddress\": \"$1\"}}";
-        return payload.replaceAll(regexOwnerAddress, replacementOwnerAddress).replaceAll(regexOperatorAddress, replacementOperatotAddress);
+        return output.toString();
     }
-    
+
+    public static String fixExpenseItemAndSubType(String payload) {
+        // The following code aims to replace occurrences of "expenseItem": {} with
+        // "expenseItem": "", as "expenseItem"
+        // is expected to be a String and not an object.
+        String result = payload.replaceAll("(\"expenseItem\":\\s*)\\{\\}", "$1\"\"");
+        // The following code aims to replace occurrences of "expenseItemSubType": {}
+        // with "expenseItemSubType": "", as "expenseItemSubType"
+        // is expected to be a String and not an object.
+        result = result.replaceAll("(\"expenseItemSubType\":\\s*)\\{\\}", "$1\"\"");
+        return result;
+    }
+
+    public static String ltcFacilityBackwardCompatibility(String payload) {
+        String regexOwnerAddress = "\"ownerAddress\":\\s*\"([^\"]*)\"";
+        String replacementOwnerAddress = "\"ownerAddress\": {\"geometry\": {\"coordinates\": [0, 0]}, \"properties\": {\"fullAddress\": \"$1\"}}";
+        String regexOperatorAddress = "\"operatorAddress\":\\s*\"([^\"]*)\"";
+        String replacementOperatotAddress = "\"operatorAddress\": {\"geometry\": {\"coordinates\": [0, 0]}, \"properties\": {\"fullAddress\": \"$1\"}}";
+        return payload.replaceAll(regexOwnerAddress, replacementOwnerAddress).replaceAll(regexOperatorAddress,
+                replacementOperatotAddress);
+    }
+
     public static String fixPcnName(String payload) {
-        // The following code aims to replace occurences of "pcn":"[]" with "pcn": "", as "pcn" is expected to be
+        // The following code aims to replace occurences of "pcn":"[]" with "pcn": "",
+        // as "pcn" is expected to be
         // a String array and not an array
         String pcnPattern = "\"pcnName\":\\[\\]";
         String pcnReplacement = "\"pcnName\": \"\"";
