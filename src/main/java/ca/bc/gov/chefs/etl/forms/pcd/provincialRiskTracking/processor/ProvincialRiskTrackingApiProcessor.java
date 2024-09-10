52e3a9f65557277fb4ff06a1f9b38143a0565550
Dinh Nguyen Pham <nguyenphamswork@gmail.com>
2024-09-10 08:37:08 -0700

    fix: adjusted indentation in API processor

--- src/main/java/ca/bc/gov/chefs/etl/forms/pcd/provincialRiskTracking/processor/ProvincialRiskTrackingApiProcessor.java
+++ src/main/java/ca/bc/gov/chefs/etl/forms/pcd/provincialRiskTracking/processor/ProvincialRiskTrackingApiProcessor.java
@@ -4,7 +4,7 @@ import ca.bc.gov.chefs.etl.constant.PCDConstants;
 import ca.bc.gov.chefs.etl.core.processor.BaseApiProcessor;
 
 public class ProvincialRiskTrackingApiProcessor extends BaseApiProcessor {
-    	public ProvincialRiskTrackingApiProcessor() {
+    public ProvincialRiskTrackingApiProcessor() {
 		this.formPropertyName = PCDConstants.PCD_PROVINCIAL_RISK_TRACKING_PROPERTY;
 	}
 }
