e8eed249ab5d60cf3d8f752a23e8a03e85f452bd
Dinh Nguyen Pham <nguyenphamswork@gmail.com>
2024-11-07 19:37:55 -0800

    adding the facility mapping form etl and editting facility information

--- src/main/java/ca/bc/gov/chefs/etl/ChefsETLMainMethod.java
+++ src/main/java/ca/bc/gov/chefs/etl/ChefsETLMainMethod.java
@@ -30,6 +30,7 @@ import ca.bc.gov.chefs.etl.forms.pcd.statusTracker.route.StatusTrackerFormRoute;
 import ca.bc.gov.chefs.etl.forms.pcd.upcc.budget.route.UpccBudgetFormRoute;
 import ca.bc.gov.chefs.etl.forms.pcd.upcc.financialReporting.route.UpccFRFormRoute;
 import ca.bc.gov.chefs.etl.forms.pcd.upcc.pcPatientServices.route.UpccPcpsFormRoute;
+import ca.bc.gov.chefs.etl.forms.pda.facilityMapping.route.FacilityMappingRoute;
 import ca.bc.gov.chefs.etl.forms.pda.waitTime.route.WaitTimeRoute;
 
 /**
@@ -81,6 +82,7 @@ public class ChefsETLMainMethod {
 
 		/* --------------PDA ROUTES --------------- */
 		main.configure().addRoutesBuilder(WaitTimeRoute.class);
+		main.configure().addRoutesBuilder(FacilityMappingRoute.class);
 		main.run(args);
 	}
 }
